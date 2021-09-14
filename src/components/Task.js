1ae1ad84497f08cca9950742a1271c88a20cb640
Csaba Balint <csababalint@Csabas-MacBook-Pro-2.local>
2021-09-14 19:24:41 +0100

    Add toggleReminder function

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,8 @@
 import { FaTimes } from 'react-icons/fa';
 
-function Task({ task, onDelete }) {
+function Task({ task, onDelete, onToggle }) {
     return (
-        <div className='task' style={{position: 'relative'}}> 
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}  style={{position: 'relative'}}> 
                 <FaTimes onClick={() => onDelete(task.id)} style={{ color: 'red', cursor: 'pointer', position: 'absolute', right: '5px', top: '2px'}}/>
             <h1>
                 {task.text}
