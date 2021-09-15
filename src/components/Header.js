650252bbd9320764ae32fca1a3e829b78e3e5962
Csaba Balint <csababalint@Csabas-MacBook-Pro-2.local>
2021-09-15 19:02:43 +0100

    Add AddTask Component, Add Hide and Show Button for AddTask component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,15 @@ import Button from './Button';
 
 
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click from header')
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
         <header className='header'>
             <h1 >{title}</h1>
-            <Button color="green" text='Add' onClick={onClick}/>
+            <Button
+                color={showAdd ? 'red' : 'green'}
+                text={showAdd ? 'Close' : 'Add'}
+                onClick={onAdd} />
         </header>
     )
 }
